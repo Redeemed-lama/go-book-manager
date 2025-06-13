845e6d706c9f760e0b2eae5f8d0d3cb0ee262607
Redeemed-lama <mahmoudalpha18@gmail.com>
2025-06-13 16:25:33 +0100

    Updated main.go and added note.txt

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, Git and Go!")
+	readFile, err := os.Open("note.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer readFile.Close()
+	scanner := bufio.NewScanner(readFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "done" {
+			break
+		}
+		fmt.Println("all notes saved")
+		fmt.Println("You wrote:", line)
+	}
+
 }
